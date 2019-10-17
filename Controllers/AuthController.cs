aad70d2d0eb78facb98f897e5858964119732065
‘Richard <richie_folab@yahoo.com>
2019-10-17 06:57:00 +0100

    second commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,97 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {        private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
+        {
+            _config = config;
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register (UserForRegisterDto userForRegister) {
+
+            //convert user name to lower case
+            userForRegister.Username = userForRegister.Username.ToLower();
+
+            if (await _repo.UserExists(userForRegister.Username)) 
+                return BadRequest("Username already exists");
+            
+
+            //create a user object
+            var userToCreate = new User 
+            {
+                Username = userForRegister.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegister.Password);
+
+
+            return StatusCode(201);
+
+        } 
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            //Ensure username and password matches what is stored in the db
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            //If there is not matches, return Http status code 401
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            //The taken will contain two claims (User Id and User Name)
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            //Create a security key. The key is read from the config file
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+
+            //Use the security key as part of the signing credentials and encrypt the key with SHA512 hashing algorithm
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            //Create a token descriptor and pass the claims as the subject, provide expiry date and the sign the credential
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            //create token handler
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            //create the token
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            //write the token to the response sent back to the client
+            return Ok(new {
+                token = tokenHandler.WriteToken(token)
+            });
+        }
+
+    }
+
+
+}
--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,7 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite (Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>(); //AddScoped creates an instance for each http request
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
